c492cad4966a658bd4298e083f04aea81e8102b7
a.samat.dev <a.samat.dev@gmail.com>
2023-06-19 08:47:53 +0600

    Test fix

--- src/test/java/kz/smarthealth/scheduleservice/controller/ScheduleControllerTest.java
+++ src/test/java/kz/smarthealth/scheduleservice/controller/ScheduleControllerTest.java
@@ -36,7 +36,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
 @TestPropertySource(locations = "classpath:application-test.yml")
 class ScheduleControllerTest {
 
